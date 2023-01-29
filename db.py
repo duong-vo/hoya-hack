b4c9989fe00d78087b5a9fcb176506f0dd97feb0
dominhnhut01 <dominhnhut01@gmail.com>
2023-01-29 00:10:44 -0500

    reinitialize database

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import random
 
 def connection():
     try:
@@ -11,6 +11,7 @@ def connection():
 conn = connection()
 cursor = conn.cursor()
 
+
 # Create Terns table
 cursor.execute("""CREATE TABLE IF NOT EXISTS Items (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,4 +19,15 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS Items (
                     price INTEGER
                     );""")
 
+items = ['adidas_cap', 'american_crew_hair_paste', 'coconut_water', 'oven_mitt',
+         'pink_ukelele', 'scissors', 'spoon', 'teddy_hamster', 'water_bottle', 'computer_mouse',
+         'disinfecting_cleaner', 'febreeze_spray', 'fork', 'keyboard', 'knife', 'ladle', 'nutella',
+         'omachi_mi_bap_bo', 'omachi_sot_bo_ham', 'phone', 'shear_revival_clay_pomade', 'skippy_peanut_butter',
+         'sprite', 'teddy_octopus', 'watch', 'wine']
+
+for item in items:
+    price = random.randint(2, 50)
+    cursor.execute("INSERT INTO Items (item, price) VALUES (?, ?)",
+                    (item, price))
+    conn.commit()
 conn.close()
